7870dd343470dd9659fcb9e338866fb5a5d9d5a1
Luke Smith <lukedansmi@sbcglobal.net>
2016-09-04 13:12:11 -0500

    New function gclass() to predict column classes within a file - particularly CSV.

---
+++ R/import.R
@@ -0,0 +1,25 @@
+
+# gclass() ----------------------------------------------------------------
+#'
+#' Guess classes of a file.
+#'
+#' @description
+#' This is a quick-and-dirty method of guessing a file's classes (hence the
+#' g in \code{gclass()}).
+#'
+#' @param x A file to read lines from and guess its columns' classes.
+#' @param n The number of lines you want \code{gclass()} to read. Default is 6.
+#' @param ... Other parameters to pass to \code{gclass()}. See \code{Details}.
+#'
+#' @details
+#' \code{gclass()} makes an estiamte as to which read function to use. For
+#' example, if you pass the parameter \code{widths} then \code{gclass} assumes
+#' you are trying to read from a fixed-width file and so uses \code{read.fwf} to
+#' read the number of lines specified by the parameter \code{n}.
+
+gclass <- function(x, n = 6L, ...){
+  classes <- readLines(file, n = n) %>%
+    textConnection() %>%
+    read.csv(stringsAsFactors = FALSE) %>%
+    sapply(class)
+}
