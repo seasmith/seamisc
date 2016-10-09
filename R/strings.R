143bf61cbbedbda2747144e61cea57fc5e63e8f7
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-10-09 18:41:08 -0500

    Add new paste function

--- R/strings.R
+++ R/strings.R
@@ -1,10 +1,19 @@
 
+# paste() -----------------------------------------------------------------
+#
+#' Paste unqouted values and return unqouted name/symbol
+paste00 <- function(..., sep = "", collapse = NULL){
+    dots <- as.list(substitute(list(...)))[-1L]
+    dots.pasted <- paste(dots, sep = sep, collapse = collapse)
+    dots.name <- as.name(dots.pasted)
+}
+
 # camelize() --------------------------------------------------------------
 #
-#' Convert snake_case to CamelCase
+#' Convert snake_case to CamelCase.
 #'
 #' @seealso
-#' Taken from ~/ggplot2/utilities.r
+#' Taken from ~/ggplot2/utilities.r.
 camelize <- function(x, first = FALSE) {
   x <- gsub("_(.)", "\\U\\1", x, perl = TRUE)
   if (first) x <- firstUpper(x)
@@ -15,10 +24,10 @@ camelize <- function(x, first = FALSE) {
 
 # snakeize() --------------------------------------------------------------
 #
-#' Convert CamelCase to snake_case
+#' Convert CamelCase to snake_case.
 #'
 #' @seealso
-#' Taken from ~/ggplot2/utilities.r
+#' Taken from ~/ggplot2/utilities.r.
 snakeize <- function(x) {
   x <- gsub("([A-Za-z])([A-Z])([a-z])", "\\1_\\2\\3", x)
   x <- gsub(".", "_", x, fixed = TRUE)
