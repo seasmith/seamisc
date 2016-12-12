1412971a3e120853bf697682326148532ac80ce2
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-12-12 13:49:02 -0600

    v 0.2.0 - add add_and() for printing with knitr

--- R/strings.R
+++ R/strings.R
@@ -6,16 +6,41 @@
 #'
 #' @param string A character vector or a factor vector. Factors will be
 #'   converted to character.
+#' @param oxford Logical. Should the output string use the Oxford comma or not.
+#' @param and Character vector indicating which style of "and" to use at the end
+#'   of the output string. '\\b' indicates that there is to be no "and", which
+#'   instead inserts a literal backspace.
+#' @param sep Character vector indicating the separator to use. Could be the
+#'   usual comma (","), semi-colon (";"), or something else.
 #'
 #' @export
 
 
-add_and <- function(string, and = c("and", "&")) {
+add_and <- function(string, oxford = TRUE, and = c("and", "&", "\\b"), sep = c(",", ";")) {
+  # Check - String must be a factor or character vector.
   stopifnot(is.character(string) | is.factor(string))
   if (is.factor(string)) as.character(string)
-  len <- length(string)
-  new.string <- c(string[1:(len -1)], paste(and[1], string[len]))
-  return(new.string)
+
+  # Inputs - Set length arguments.
+  #        - Make paste easier by setting comma <- NULL if oxford == FALSE.
+  #        - Set and <- "\\b" if there string is length 1 seq.len_1[1] == 0/FALSE
+  len   <- length(string)
+  len_1 <- len - 1
+  seq.len_1 <- if (len_1) 1:len_1 else 0
+
+  sep_space   <- paste0(sep[1], " ")
+  sep_nospace <- if (oxford) sep[1] else NULL
+  sep_nospace <- if (len > 2) sep_nospace else NULL
+
+  and <- if (len == 1) "\\b" else and[1]
+
+  # Input - First half of new string no matter the length or
+  #       - if oxford == TRUE/FALSE.
+  ns_first  <- paste(string[seq.len_1], collapse = sep_space)
+  ns_second <- paste(sep_nospace, and, string[len])
+  ns <- paste0(ns_first, ns_second)
+
+  return(ns)
 }
 
 
