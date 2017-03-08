fb940b153cb0926eb47e41fc0a8d3e1752c5b966
Luke Smith <lukedansmi@sbcglobal.net>
2017-03-08 10:55:47 -0600

    Create ggraph-naniar helper functions

--- R/dataFrames.R
+++ R/dataFrames.R
@@ -1,6 +1,168 @@
 #' @include utils.dataFrames.R
 NULL
 
+
+
+# graph_miss_col() ------------------------------------------------------------
+
+graph_miss_col <- function(x) {
+  nms <- names(x)
+  ncl <- ncol(x)
+  nrw <- nrow(x)
+  
+  df_edges <- data.frame(parent = c(rep("!NA", ncl), rep("NA", ncl), "data", "data"),
+                         child  = c(paste0(nms, "_!NA"), paste0(nms, "_NA"), "!NA", "NA"))
+  # data.frame(parent = c(rep("data", ncl*2)),
+  #                         child  = c(paste0(nms, "_!NA"), paste0(nms, "_NA")))
+  
+  
+  nms_rep <- vapply(nms, rep, character(2), 2)
+  nms_rep <- as.character(nms_rep)
+  
+  nms_apnd <- vapply(nms,
+                     function(x) c(paste0(x, "_!NA"), paste0(x, "_NA")),
+                     character(2))
+  nms_apnd <- as.character(nms_apnd)
+  
+  df_edges2 <- tibble::tibble(parent = c(rep("data", ncl),
+                            nms_rep),
+                 child  = c(nms,
+                            nms_apnd))
+  
+  # Create missing data summary
+  df_miss <- naniar::miss_var_summary(x)
+  df_miss <- dplyr::select(df_miss, -percent)
+  df_miss <- dplyr::rename(df_miss, size = n_missing)
+
+  # Create present data summary
+  df_pres <- dplyr::tibble(variable = df_miss$variable,
+                              size     = nrw - df_miss$size)
+
+  # Remove 0's
+  df_miss    <- dplyr::filter(df_miss, size != 0)
+  df_pres <- dplyr::filter(df_pres, size != 0)
+  
+  # Append appropriate modifiers to the end of variables
+  df_miss <- dplyr::mutate(df_miss, col = variable, variable = paste0(variable, "_NA"))
+  df_pres <- dplyr::mutate(df_pres, col = variable, variable = paste0(variable, "_!NA"))
+  
+  # Add summary row for each table
+  df_miss <- dplyr::add_row(.data    = df_miss,
+                            variable = "NA",
+                            size     = 0)
+  df_pres <- dplyr::add_row(.data       = df_pres,
+                               variable = "!NA",
+                               size     = 0)
+  
+  nrw_miss <- nrow(df_miss)
+  nrw_pres <- nrow(df_pres)
+  
+  df_miss <- tibble::add_column(.data = df_miss,
+                                value = rep("NA", nrw_miss))
+  df_pres <- tibble::add_column(.data = df_pres,
+                                value = rep("!NA", nrw_pres))
+  
+  df_vertices <- dplyr::union(df_miss, df_pres)
+
+  df_vertices <- dplyr::add_row(.data    = df_vertices,
+                                variable = "data",
+                                size     = 0,
+                                value = "data")
+  
+  df_vertices2 <- dplyr::filter(df_vertices, variable != "!NA")
+  df_vertices2 <- dplyr::filter(df_vertices2, variable != "NA")
+  df_vertices2 <- dplyr::add_row(.data    = df_vertices2,
+                                 variable = nms,
+                                 size     = 0,
+                                 col      = NA,
+                                 value    = NA)
+  df_vertices2 <- dplyr::semi_join(df_edges2, df_vertices2, by = c("child" = "variable"))
+  
+  df_edges2 <- dplyr::semi_join(df_edges2, df_vertices2, by = c("child" = "variable"))
+
+  graph <- igraph::graph_from_data_frame(df_edges, vertices = df_vertices)
+  
+  graph2 <- igraph::graph_from_data_frame(df_edges2, vertices = df_vertices2)
+
+  graphs <- lapply(list(graph, graph2), function(x) {
+    x <- ggraph::tree_apply(x, function(node, parent, depth, tree) {
+      tree <- igraph::set_vertex_attr(tree, 'depth', node, depth)
+      if (depth == 1) {
+        tree <- igraph::set_vertex_attr(tree, 'class', node, igraph::V(tree)$value[node])
+      } else if (depth > 1) {
+        tree <- igraph::set_vertex_attr(tree, 'class', node, igraph::V(tree)$class[parent])
+      }
+      tree
+    })
+    
+  })
+  
+  names(graphs) <- c("by_presence", "by_column")
+  
+  igraph::V(graphs$by_presence)$leaf <- igraph::degree(graphs$by_presence, mode = 'out') == 0
+  igraph::V(graphs$by_column)$leaf <- igraph::degree(graphs$by_column, mode = 'out') == 0
+
+}
+
+
+
+# gg_miss_treemap() -------------------------------------------------------
+
+gg_miss_treemap <- function(graphs) {
+  ggraph(graphs$by_presence, 'treemap', weight = "size", width = 1, height = 3) +
+    geom_node_tile(aes(filter = leaf, fill = forcats::fct_rev(class)), colour = NA) +
+    geom_node_tile(aes(filter = depth != 0, size = depth), fill = NA) +
+    geom_node_text(aes(label = col), repel = TRUE) +
+    scale_alpha(range = c(1, 0.3), guide = 'none') +
+    scale_size(range = c(1.5, 0.4), guide = 'none')
+  
+  ggraph(graphs$by_column, 'treemap', weight = "size", width = 1, height = 3) +
+    geom_node_tile(aes(filter = leaf, fill = forcats::fct_rev(value)), colour = NA) +
+    geom_node_tile(aes(filter = depth != 0, size = depth), fill = NA) +
+    geom_node_text(aes(label = col)) +
+    scale_alpha(range = c(1, 0.3), guide = 'none') +
+    scale_size(range = c(1.5, 0.4), guide = 'none') + coord_flip()
+  
+}
+
+
+
+
+# graph_miss_cat() --------------------------------------------------------
+
+graph_miss_cat <- function(x, ...) {
+  dots <- as.list(substitute(list(...)))[-1L]
+  chars <- as.character(dots)
+  len <- length(chars)
+  
+  # x %>%
+  #   split(.[, dots]) %>%
+  #   map(naniar::miss_var_summary) %>%
+  #   plyr::ldply()  
+  
+  x %>%
+    group_by_(groups = chars) %>%
+    dmap(function(x) sum(is.na(x))) %>%
+    gather(... = -groups, "variable", "n_missing") %>%
+    mutate(percent = (n_missing / nrow(x) * 100)) %>%
+    arrange(-n_missing)
+}
+
+
+
+
+
+# at() --------------------------------------------------------------------
+
+#' Wrapper for \code{tibble::as_tibble()}.
+#' 
+#' @param x A data frame to convert to a tibble.
+#' 
+#' @export
+at <- function(x) {
+  tibble::as_tibble(x)
+}
+
 # getColNums() ------------------------------------------------------------ '
 #'
 #' Returns columns numbers using dplyr-like selection syntax (i.e. col1:col5).
